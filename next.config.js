59815c0924009d4424b177a9ac06c4ef1cd948cb
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-01-30 12:07:40 +0500

    node-loader

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
-        config.module.rules.push({
-            test: /\.node$/,
-            use: [
-                {
-                    loader: "nextjs-node-loader",
-                    options: {
-                        flags: os.constants.dlopen.RTLD_NOW,
-                        outputPath: config.output.path,
-                    },
-                },
-            ],
-        });
-        return config;
+    target: "node",
+    node: {
+      __dirname: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.node$/,
+          loader: "node-loader",
+        },
+      ],
     },
 };
 
