750a0983d983b59b2a38b1d61c9fcbe61565e455
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-03-19 21:35:56 +0500

    changed sidebar navigation

--- src/app/(DashboardLayout)/layout/sidebar/NavGroup/NavGroup.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/NavGroup/NavGroup.tsx
@@ -1,35 +1,75 @@
-import PropTypes from 'prop-types';
 // mui imports
-import { ListSubheader, styled, Theme } from '@mui/material';
-
+import {
+    Collapse,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    styled,
+    useTheme,
+} from "@mui/material";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
+import { MenuItem } from "../SidebarItems";
+import { useState } from "react";
 type NavGroup = {
-  navlabel?: boolean;
-  subheader?: string;
+    navlabel?: boolean;
+    subheader?: string;
+    items: any;
 };
 
-interface ItemType {
-  item: NavGroup;
-}
-
-const NavGroup = ({ item }: ItemType) => {
-  const ListSubheaderStyle = styled((props: Theme | any) => <ListSubheader disableSticky {...props} />)(
-    ({ theme }) => ({
-      ...theme.typography.overline,
-      fontWeight: '700',
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(0),
-      color: theme.palette.text.primary,
-      lineHeight: '26px',
-      padding: '3px 12px',
-    }),
-  );
-  return (
-    <ListSubheaderStyle>{item.subheader}</ListSubheaderStyle>
-  );
-};
-
-NavGroup.propTypes = {
-  item: PropTypes.object,
+const NavGroup = ({ item, onClick }: any) => {
+    const theme = useTheme();
+    const { items: children } = item;
+    const Icon = item.icon;
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen((prev) => !prev);
+    };
+    const ListItemStyled = styled(ListItem)(() => ({
+        whiteSpace: "nowrap",
+        marginBottom: "8px",
+        cursor: "pointer",
+        padding: "8px 10px",
+        borderRadius: "8px",
+        color: theme.palette.text.secondary,
+        paddingLeft: "10px",
+        "&:hover": {
+            backgroundColor: theme.palette.primary.light,
+            color: theme.palette.primary.main,
+        },
+        "&.Mui-selected": {
+            color: "white",
+            backgroundColor: theme.palette.primary.main,
+            "&:hover": {
+                backgroundColor: theme.palette.primary.main,
+                color: "white",
+            },
+        },
+    }));
+    return (
+        <List>
+            <ListItemStyled onClick={handleClick}>
+                <ListItemIcon
+                    sx={{
+                        minWidth: "36px",
+                        p: "3px 0",
+                        color: "inherit",
+                    }}
+                >
+                    {item.icon ? <Icon stroke={1.5} size="1.3rem" /> : <></>}
+                </ListItemIcon>
+                <ListItemText primary={item.title} />
+                {open ? <IconChevronUp /> : <IconChevronDown />}
+            </ListItemStyled>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    {children.map((child: any, key: any) => (
+                        <MenuItem key={key} item={child} onclick={onClick} />
+                    ))}
+                </List>
+            </Collapse>
+        </List>
+    );
 };
 
 export default NavGroup;
--- src/app/(DashboardLayout)/layout/sidebar/SidebarItems.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/SidebarItems.tsx
@@ -5,32 +5,46 @@ import { Box, List } from "@mui/material";
 import NavItem from "./NavItem";
 import NavGroup from "./NavGroup/NavGroup";
 
-const SidebarItems = ({ toggleMobileSidebar }: any) => {
+function hasChildren(item: any) {
+    const { items: children } = item;
+
+    if (children === undefined) {
+        return false;
+    }
+
+    if (children.constructor !== Array) {
+        return false;
+    }
+
+    if (children.length === 0) {
+        return false;
+    }
+
+    return true;
+}
+
+export const MenuItem = ({ item, onClick }: any) => {
   const pathname = usePathname();
   const pathDirect = pathname;
-  return (
-    <Box sx={{ px: 2 }}>
-      <List sx={{ pt: 0 }} className="sidebarNav" component="div">
-        {Menuitems.map((item) => {
-          // {/********SubHeader**********/}
-          if (item.subheader) {
-            return <NavGroup item={item} key={item.id} />;
-
-          //   // {/********If Sub Menu**********/}
-          //   /* eslint no-else-return: "off" */
-          } else {
-            return (
-              <NavItem
-                item={item}
-                key={item.id}
-                pathDirect={pathDirect}
-                onClick={toggleMobileSidebar}
-              />
-            );
-
-}})}
-      </List>
-    </Box>
+  return hasChildren(item) ? (
+    <NavGroup item={item} key={item.id} onClick={onClick} />
+  ) : (
+    <NavItem
+      item={item}
+      key={item.id}
+      pathDirect={pathDirect}
+      onClick={onClick}
+    />
   );
+};
+
+const SidebarItems = ({ toggleMobileSidebar }: any) => {
+    return (
+        <Box sx={{ px: 2 }}>
+            <List sx={{ pt: 0 }} className="sidebarNav" component="div">
+                {Menuitems.map((item, key) => <MenuItem key={key} item={item} onClick={toggleMobileSidebar}/>)}
+            </List>
+        </Box>
+    );
 };
 export default SidebarItems;
