d29462434aeab173d94b1006a1b119ed17ebb322
Afnan Riaz <hafiz.afnan322@gmail.com>
2024-06-10 20:55:18 +0500

    cdn configuarion

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises";
 import { NextResponse } from "next/server";
+import path from 'path';
 
 export async function POST(req: any) {
     const data = await req.formData();
@@ -9,20 +10,22 @@ export async function POST(req: any) {
     }
     let fileFolder;
     if (file.type.startsWith('image/')) {
-        fileFolder = 'images';
+        fileFolder = '/images/';
     } else if (file.type.startsWith('audio/')) {
-        fileFolder = 'audio';
+        fileFolder = '/audio/';
     } else if (file.type.startsWith('video/')) {
-        fileFolder = 'videos';
+        fileFolder = '/videos/';
     } else {
         return NextResponse.json({ message: "unsupported file type", success: false });
     }
     const byteData = await file.arrayBuffer();
     const buffer = Buffer.from(byteData);
-    const path = `./public/${fileFolder}/${file.name}`;
-    await writeFile(path, buffer);
 
-    const location = `/${fileFolder}/${file.name}`;
+    // Change this path to your CDN path
+    const cdnPath = path.join('/var/www/assets', fileFolder, file.name);
+    await writeFile(cdnPath, buffer);
+
+    const location = `https://cdn.jamiat.org.pk${fileFolder}${file.name}`;
     return NextResponse.json({
         location: location,
         message: "file uploaded",
